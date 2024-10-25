6eb19f58cf35842e3e98e061cff590c4e6eaa3cb
Joshua Moore <Jmoore0177@conestogac.on.ca>
2024-10-25 19:35:10 -0400

    Forgot to switch branches

--- app/src/main/java/com/example/mtgscannerproject/CameraActivity.kt
+++ app/src/main/java/com/example/mtgscannerproject/CameraActivity.kt
@@ -17,10 +17,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.example.mtgscannerproject.ui.theme.MTGScannerProjectTheme
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.TextRecognizerOptionsInterface
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 class CameraActivity : ComponentActivity() {
 
     private lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var imageAnalyzer: ImageAnalysis
+    private val recognizer: TextRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+    private var lastProcessedTime = System.currentTimeMillis()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,13 +74,28 @@ class CameraActivity : ComponentActivity() {
                 it.setSurfaceProvider(previewView.surfaceProvider)
             }
 
+            // Now Setup the ImageAnalysis UseCase
+            imageAnalyzer = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also { analysisUseCase ->
+                    analysisUseCase.setAnalyzer(ContextCompat.getMainExecutor(this)) { imageProxy ->
+                        processImageProxy(imageProxy)
+                    }
+                }
+
             // Select back camera
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             // Bind the camera to the lifecycle
             val useCaseGroup = UseCaseGroup.Builder().addUseCase(previewUseCase).build()
-            cameraProvider.bindToLifecycle(this, cameraSelector, useCaseGroup)
+            cameraProvider.bindToLifecycle(this, cameraSelector, useCaseGroup, imageAnalyzer)
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun processImageProxy(imageProxy: ImageProxy) {
+        val currentTime = System.currentTimeMillis()
+        if(currentTime - lastProcessedTime)
     }
 
     // Additional methods for handling camera and ML Kit functionality...
