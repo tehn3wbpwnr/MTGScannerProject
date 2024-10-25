916fafcb4c824718009008a00df70b49b2c35f10
Joshua Moore <Jmoore0177@conestogac.on.ca>
2024-10-25 16:21:31 -0400

    Camera permissions added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Plugins using version catalogs
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
